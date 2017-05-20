27f1ddfb904840f886e88628e987cb6f022af2c9
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-05-20 11:09:42 +0530

    index.js file updated and procfile added

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 const express = require('express');
-let app = express()
-let timeInMs = Date.now();
+let app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-console.log(timeInMs)
-
-let ts = Math.round((new Date()).getTime() / 1000);
-
-console.log(ts)
-
-let date = new Date(ts*1000)
-
-console.log(String(date))
-
-const year = String(date).substring(11,15)
-const month = String(date).substring(4,7)
-const day = String(date).substring(8,10)
-
-console.log(month + " " + day + "," + year)
-
-const dateObj = {
-   "unix": ts,
-   "natural": month + " " + day + "," + year
-}
-
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+let dateObj = {};
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/index.html'));
 })
 
+let d = new Date(1167436800)
+console.log(d)
+
 app.get('/:input', (req, res) => {
-  const date = req.url.substring(1)
-  
-  console.log(date)
-  res.send("fuck u");
+  const input = req.url.substring(1)
+  console.log(input + new Date(parseInt(input) * 1000) + new Date(input))
+  if (new Date(parseInt(input) * 1000) == 'Invalid Date' || new Date(input) == 'Invalid Date') {
+    dateObj = {
+      "unix": null,
+      "natural": null
+    }
+  } else {
+    if (input.search(/\d{10}/) != -1) {
+      const ts = new Date(parseInt(input) * 1000)
+      const day = ts.getDate();
+      const month = ts.getMonth() + 1;
+      const year = ts.getFullYear();
+      dateObj = {
+        "unix": input,
+        "natural": month + " " + day + "," + year
+      }
+    } else {
+      const ts = new Date(input)
+      const day = ts.getDate();
+      const month = ts.getMonth() + 1;
+      const year = ts.getFullYear();
+      dateObj = {
+        "unix": ts.getTime()/1000,
+        "natural": month + " " + day + "," + year
+      }
+    }
+  }
+  // let d = Date.parse(month + " " + day + "," + year) / 1000;
+  // console.log(d)
+
+  res.send(dateObj);
 })
 
 
 app.listen(port, (req, err) => {
-  if(!err)
+  if (!err)
     console.log("listening to the port " + port)
 })
