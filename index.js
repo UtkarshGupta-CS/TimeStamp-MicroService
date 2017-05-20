891fd39dd49c8c96ebc2ec1b27b1922ecbe825c5
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-05-20 15:01:30 +0530

    minor changes in index.html file

--- index.js
+++ index.js
@@ -3,14 +3,55 @@ let app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 let dateObj = {};
+
+const monthConv = (monthNum) => {
+  switch (monthNum) {
+    case 1:
+      return "January";
+
+    case 2:
+      return "February";
+
+    case 3:
+      return "March";
+
+    case 4:
+      return "April";
+
+    case 5:
+      return "May";
+
+    case 6:
+      return "June";
+
+    case 7:
+      return "July";
+
+    case 8:
+      return "August";
+
+    case 9:
+      return "September";
+
+    case 10:
+      return "October";
+
+    case 11:
+      return "November";
+
+    case 12:
+      return "December";
+
+    default:
+      break;
+  }
+}
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/index.html'));
 })
 
-let d = new Date(1483056000000)
-console.log(d)
-
-app.get('/:input', (req, res) => {
+app.get('/:query', (req, res) => {
   const input = decodeURI(req.url.substring(1));
   console.log(input + new Date(parseInt(input) * 1000) + new Date(input))
   if (new Date(parseInt(input) * 1000) == 'Invalid Date' && new Date(input) == 'Invalid Date') {
@@ -25,22 +66,27 @@ app.get('/:input', (req, res) => {
       const month = ts.getMonth() + 1;
       const year = ts.getFullYear();
       dateObj = {
-        "unix": input,
-        "natural": month + " " + day + "," + year
+        "unix": parseInt(input),
+        "natural": monthConv(month) + " " + day + ", " + year
       }
     } else {
       const ts = new Date(input)
       const day = ts.getDate();
       const month = ts.getMonth() + 1;
       const year = ts.getFullYear();
-      dateObj = {
-        "unix": ts.getTime()/1000,
-        "natural": month + " " + day + "," + year
+      if (ts != 'Invalid Date') {
+        dateObj = {
+          "unix": ts.getTime() / 1000,
+          "natural": monthConv(month) + " " + day + ", " + year
+        }
+      } else {
+        dateObj = {
+          "unix": null,
+          "natural": null
+        }
       }
     }
   }
-  // let d = Date.parse(month + " " + day + "," + year) / 1000;
-  // console.log(d)
 
   res.send(dateObj);
 })
